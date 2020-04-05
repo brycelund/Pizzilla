ad8f6c0085574d07b6a1f91ae50315ccafabecd3
Bryce Lund <bryce.lund@gmail.com>
2020-04-04 21:46:39 -0600

    Implement stripe

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,7 @@
 // Bring in express so we can use it
 const express = require('express')
+//Bring in Stripe
+const stripe = require('stripe')('STRIPE_SECRET_KEY')
 // Set up the router
 const router = express.Router()
 
@@ -12,7 +14,7 @@ const Order = require('./models/Order')
 // /api because that is where we mounted this router in app.js
 // /test because that's what is written right here below
 // Anytime someone visits localhost:3000/api/test, we'll run this function
-router.get('/test', function(req, res, next) {
+router.get('/test', function (req, res, next) {
   res.send('testing endpoint')
 })
 
@@ -22,7 +24,7 @@ router.get('/test', function(req, res, next) {
 
 // GET /orders - Return all of our orders
 // This function is marked as an async function so that we can use await inside of it
-router.get('/orders', async function(req, res, next) {
+router.get('/orders', async function (req, res, next) {
   // Look in our database in our Orders collection for any record (This will bring back all records). Wait to find them all before proceeding.
   const allOrders = await Order.find({})
   // Send back all of our orders in the form of JSON
@@ -30,7 +32,7 @@ router.get('/orders', async function(req, res, next) {
 })
 
 // GET /orders/id - Return info on a specific order
-router.get('/orders/:id', async function(req, res, next) {
+router.get('/orders/:id', async function (req, res, next) {
   // Get the id from the URL
   const orderID = req.params.id
   // Look in our database in our Orders collection for a record with an _id that matches orderID. Wait to find one before proceeding.
@@ -40,7 +42,7 @@ router.get('/orders/:id', async function(req, res, next) {
 })
 
 // POST /orders - Create a new order
-router.post('/orders', function(req, res, next) {
+router.post('/orders', function (req, res, next) {
   // Create a new Order object
   const newOrder = new Order()
   newOrder.customer = req.body.customer
@@ -55,7 +57,7 @@ router.post('/orders', function(req, res, next) {
 })
 
 // PUT /orders/id - Update an order
-router.put('/orders/:id', async function(req, res, next) {
+router.put('/orders/:id', async function (req, res, next) {
   // Get the id from the URL ^ :id
   const orderID = req.params.id
   // Look for an order with the _id of orderID, then update it to be whatever we sent in the body of our request.
@@ -63,6 +65,34 @@ router.put('/orders/:id', async function(req, res, next) {
   let updateOrder = await Order.updateOne({ _id: orderID }, req.body)
   // Send back the HTTP 200 response. This means all good. 👍
   res.sendStatus(200)
+})
+
+router.post('/checkout', function (req, res, next) {
+  const BASE_PRICE = 12.0
+  const COST_PER_INGREDIENT = 0.5
+
+  let pizza_price = BASE_PRICE
+  const ingredients = req.body.toppings
+  pizza_price += ingredients.length * COST_PER_INGREDIENT
+
+  console.log(pizza_price, req.body)
+
+  stripe.charges.create(
+    {
+      amount: pizza_price * 100, // This is in cents, so x 100
+      currency: 'usd',
+      source: req.body.token,
+      description: 'Pizzilla Pizza for ' + req.body.customer,
+    },
+    function (err, charge) {
+      if (err) {
+        console.log(err)
+        res.status(400).send(err)
+        return
+      }
+      res.status(200).send(charge)
+    }
+  )
 })
 
 // Export the router for use in other files
